781e6db2fb3672db28d492641963da6bc0c29062
rajeev <rajeev.ranjan@opstree.com>
2025-11-13 11:54:31 +0530

    change version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "org.opstree.app"
         minSdk = 32 // Update to your version
         targetSdk = 35 // Update to your version
-        versionCode = 29
+        versionCode = 30
         versionName = "1.0.1"
     }
 	signingConfigs {
