b6dee62a1ca57b9e2a1b141b6af40dfcb94bcdc7
Chittorian_rajeev <ranjanrajeev886@gmail.com>
2026-03-07 12:48:47 +0530

    Update versionCode to 40 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "org.opstree.app"
         minSdk = 32 // Update to your version
         targetSdk = 35 // Update to your version
-        versionCode = 30
+        versionCode = 40
         versionName = "1.0.1"
     }
 	signingConfigs {
