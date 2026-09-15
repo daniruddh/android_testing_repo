afd70166f0d73948bf9a8936ca48fb81727926ec
aniruddh-dangarh-ops <aniruddh.dangarh@opstree.com>
2026-09-15 15:11:46 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.trueworkpoint.app"
         minSdk = 32 // Update to your version
         targetSdk = 35 // Update to your version
-        versionCode = System.getenv("VERSION_CODE")
+        versionCode = System.getenv("VERSION_CODE") ? System.getenv("VERSION_CODE").toInteger() : 1
         versionName = "1.0.1"
     }
 	signingConfigs {
